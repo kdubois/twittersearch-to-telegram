5f646f456051cf073efc4a097e559af2babe1820
Kevin Dubois <kevin.dubois@redhat.com>
2022-03-12 13:00:19 +0100

    First working version

---
+++ src/main/java/com/redhat/TweetInfoProcessor.java
@@ -0,0 +1,32 @@
+package com.redhat;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+import com.redhat.model.TweetInfo;
+
+import twitter4j.Status;
+
+public class TweetInfoProcessor implements Processor {
+
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		Status bodyIn = (Status) exchange.getIn().getBody();
+		
+		TweetInfo tweetEntity = new TweetInfo();
+		
+		tweetEntity.setTweetId(bodyIn.getId());
+		tweetEntity.setText(bodyIn.getText());
+		
+		if (bodyIn.getUser() != null) {
+			tweetEntity.setUsername(bodyIn.getUser().getName());
+			tweetEntity.setLanguage(bodyIn.getUser().getLang());
+			tweetEntity.setLocation(bodyIn.getUser().getLocation());
+		}
+
+		tweetEntity.setFavouriteCount(bodyIn.getFavoriteCount());
+		tweetEntity.setCreationDate(bodyIn.getCreatedAt());
+		
+		exchange.getIn().setBody(tweetEntity);
+	}
+
+}
---
+++ src/main/java/com/redhat/TwitterTelegramRoute.java
@@ -0,0 +1,55 @@
+package com.redhat;
+
+import javax.enterprise.context.ApplicationScoped;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.twitter.search.TwitterSearchComponent;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@ApplicationScoped
+public class TwitterTelegramRoute extends RouteBuilder {
+    @ConfigProperty(name = "twitter.apikey")
+    String twitterApiKey;
+
+    @ConfigProperty(name = "twitter.secret")
+    String twitterSecret;    
+    
+    @ConfigProperty(name = "twitter.accesstoken")
+    String twitterAccessToken;
+
+    @ConfigProperty(name = "twitter.accesstokensecret")
+    String twitterAccessTokenSecret;
+
+    @ConfigProperty(name = "telegram.token")
+    String telegramToken;
+
+    @ConfigProperty(name = "telegram.chatid")
+    String telegramChatId;
+
+    String searchTerm = "#Openshift";
+    String delay = "10000";
+    int count = 1;
+
+    @Override
+    public void configure() throws Exception {
+
+      setTwitterConfig();
+
+        fromF("twitter-search://%s?delay=%s&repeatCount=1&count=%s", searchTerm, delay, count)
+            .log(LoggingLevel.INFO, "Twitter Search Result: ${body}")
+            .process(new TweetInfoProcessor())
+            .to("telegram:bots?authorizationToken=" + telegramToken + "&chatId=" + telegramChatId);
+    }
+
+
+
+    private void setTwitterConfig(){
+        // setup Twitter component
+        TwitterSearchComponent tc = getContext().getComponent("twitter-search", TwitterSearchComponent.class);
+        tc.setAccessToken(twitterAccessToken);
+        tc.setAccessTokenSecret(twitterAccessTokenSecret);
+        tc.setConsumerKey(twitterApiKey);
+        tc.setConsumerSecret(twitterSecret);  
+    }
+
+}
